65243aa3087699bc20604833bb25b263065d03df
Sandesh Nangare <sandesh682@gmail.com>
2026-03-03 22:35:00 +0530

    .env files are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:8000";
+const API = import.meta.env.VITE_API_URL || "";
 
 function App() {
   const [todos, setTodos] = useState([]);
