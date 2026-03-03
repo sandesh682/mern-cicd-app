a59d60b3f78a1406aa1bd8187bcbd6470b4314dc
Sandesh Nangare <sandesh682@gmail.com>
2026-03-03 22:24:07 +0530

    cors issue is fixed

--- server/index.js
+++ server/index.js
@@ -1,25 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const allowedOrigins = ["http://3.26.0.207/", "http://localhost:5173"];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  }),
-);
 app.use(express.json());
 
 // MongoDB connection
